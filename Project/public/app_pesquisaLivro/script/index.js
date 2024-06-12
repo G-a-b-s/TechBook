8d47c3a09c8ba0965635abe286133672ef3e3307
Gabriel <gabrielevaine@gmail.com>
2024-06-12 15:13:11 -0300

    PesquisaLivro - tela de pesquisar
    Finalização da tela de pesquisa de livro

--- Project/public/app_pesquisaLivro/script/index.js
+++ Project/public/app_pesquisaLivro/script/index.js
@@ -1,7 +1,7 @@
 const apiUrl = '/categorias';
 
 function getCategories(processaDados) {
-    fetch(apiUrl)
+    fetch("/categorias")
         .then(response => response.json())
         .then(data => {
             processaDados(data);
@@ -9,5 +9,47 @@ function getCategories(processaDados) {
         .catch(error => {
             console.error('Erro ao ler Categorias via API JSONServer:', error);
             displayMessage("Erro ao ler Categorias");
+        });
+}
+function searchBooks(title,author,category) {
+    fetch('/livros')
+        .then(response => response.json())
+        .then(data => {
+            debugger
+            const resultsTableBody = document.querySelector('#resultsTable tbody');
+            resultsTableBody.innerHTML = '';
+            data.forEach(item => {
+                if (item.titulo.toLowerCase().includes(title)
+                    && item.autor.toLowerCase().includes(author)
+                    && (item.categoriaId == category || category == 0)) 
+                {
+                    debugger
+                    const row = document.createElement('tr');
+                    const imageCell = document.createElement('td');
+                    const titleCell = document.createElement('td');
+                    const autorCell = document.createElement('td');
+                    const descriptionCell = document.createElement('td');
+
+                    const img = document.createElement('img');
+                    img.src = item.livro;
+                    img.alt = item.titulo;
+                    img.style.width = '50px';
+
+                    imageCell.appendChild(img);
+                    titleCell.textContent = item.titulo;
+                    descriptionCell.textContent = item.descricao;
+                    autorCell.textContent = item.autor;
+
+
+                    row.appendChild(imageCell);
+                    row.appendChild(titleCell);
+                    row.appendChild(autorCell);
+                    row.appendChild(descriptionCell);
+                    resultsTableBody.appendChild(row);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
         });
 }
