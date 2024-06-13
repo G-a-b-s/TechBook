960dc0d7e16440b78fb00e01b407433d37282a12
Gustavo Souza <gustavosouza1543@outlook.com>
2024-06-12 23:08:53 -0300

    Atualização json e dashboard

---
+++ Project/public/app_dashboard/script/sdasd.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Carregar livros na página inicial
+    if (document.getElementById('book-list')) {
+        fetch('db.json')
+            .then(response => response.json())
+            .then(data => {
+                const sortedBooks = data.livros.sort((a, b) => new Date(b.data_cadastro) - new Date(a.data_cadastro));
+                const latestBooks = sortedBooks.slice(0, 5);
+
+                const bookList = document.getElementById('book-list');
+                bookList.innerHTML = ''; // Limpar a lista antes de adicionar os novos livros
+                latestBooks.forEach(book => {
+                    const colDiv = document.createElement('div');
+                    colDiv.classList.add('col-md-3', 'mb-4');
+
+                    const cardDiv = document.createElement('div');
+                    cardDiv.classList.add('card');
+
+                    const img = document.createElement('img');
+                    img.classList.add('card-img-top');
+                    img.src = book.livro;
+                    img.alt = `Capa do ${book.titulo}`;
+                    cardDiv.appendChild(img);
+
+                    const cardBody = document.createElement('div');
+                    cardBody.classList.add('card-body');
+
+                    const cardTitle = document.createElement('h5');
+                    cardTitle.classList.add('card-title');
+                    cardTitle.textContent = book.titulo;
+                    cardBody.appendChild(cardTitle);
+
+                    const cardText = document.createElement('p');
+                    cardText.classList.add('card-text');
+                    cardText.textContent = `Autor: ${book.autor}`;
+                    cardBody.appendChild(cardText);
+
+                    const cardDescription = document.createElement('p');
+                    cardDescription.classList.add('card-text');
+                    cardDescription.textContent = book.descricao;
+                    cardBody.appendChild(cardDescription);
+
+                    cardDiv.appendChild(cardBody);
+                    colDiv.appendChild(cardDiv);
+                    bookList.appendChild(colDiv);
+                });
+            })
+            .catch(error => console.error('Erro ao carregar os livros:', error));
+    }
+
+    // Carregar livros mais favoritados
+ if (document.getElementById('fav-book-list')) {
+    fetch('db.json')
+        .then(response => response.json())
+        .then(data => {
+            const books = data.livros;
+            const history = data.historico;
+            const favBooks = history.filter(item => item.favorito === "true").map(item => item.livroId);
+            const favBookElements = books.filter(book => favBooks.includes(book.id));
+            const favBookList = document.getElementById('fav-book-list');
+            
+            favBookElements.forEach(book => {
+                const colDiv = document.createElement('div');
+                colDiv.classList.add('col-md-3', 'mb-4');
+
+                const cardDiv = document.createElement('div');
+                cardDiv.classList.add('card');
+
+                const img = document.createElement('img');
+                img.classList.add('card-img-top');
+                img.src = book.livro;
+                img.alt = `Capa do ${book.titulo}`;
+                cardDiv.appendChild(img);
+
+                const cardBody = document.createElement('div');
+                cardBody.classList.add('card-body');
+
+                const cardTitle = document.createElement('h5');
+                cardTitle.classList.add('card-title');
+                cardTitle.textContent = book.titulo;
+                cardBody.appendChild(cardTitle);
+
+                const cardText = document.createElement('p');
+                cardText.classList.add('card-text');
+                cardText.textContent = `Autor: ${book.autor}`;
+                cardBody.appendChild(cardText);
+
+                const cardDescription = document.createElement('p');
+                cardDescription.classList.add('card-text');
+                cardDescription.textContent = book.descricao;
+                cardBody.appendChild(cardDescription);
+
+                cardDiv.appendChild(cardBody);
+                colDiv.appendChild(cardDiv);
+                favBookList.appendChild(colDiv);
+            });
+        })
+        .catch(error => console.error('Erro ao carregar os livros favoritados:', error));
+}
+// Event listener para o botão de início
+document.getElementById('inicio-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+});
